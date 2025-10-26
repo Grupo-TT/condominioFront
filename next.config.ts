52098e802b84cdc446133be4ee8cceae679fbaf3
SantyRamirez47 <santyrape21@gmail.com>
2025-10-26 16:35:20 -0500

    fix:fixing pr

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination:
-          'http://ec2-3-143-229-202.us-east-2.compute.amazonaws.com:8080/:path*', // tu backend AWS
-      },
-    ];
-  },
 };
 
 export default nextConfig;
