254b7d7d1f8663414c0c1b93600b3515503b895c
Miguel Andres MF <miguelandresmarinf@gmail.com>
2025-11-27 22:32:16 -0500

    Fix: turbopack settings

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const isTurbopack = Boolean(process.env.TURBOPACK)
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -9,21 +11,25 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    // Suprimir warnings de source maps de librerías de terceros
-    if (!isServer) {
-      config.ignoreWarnings = [
-        { module: /node_modules\/@hugeicons/ },
-        /Failed to parse source map/,
-      ];
-    }
-    return config;
-  },
+  ...(isTurbopack
+    ? {}
+    : {
+        webpack: (config, { isServer }) => {
+          // Suprimir warnings de source maps de librerías de terceros (solo aplica en Webpack)
+          if (!isServer) {
+            config.ignoreWarnings = [
+              { module: /node_modules\/@hugeicons/ },
+              /Failed to parse source map/,
+            ]
+          }
+          return config
+        },
+      }),
   // Suprimir warnings de source maps en la consola del navegador
   onDemandEntries: {
     maxInactiveAge: 25 * 1000,
     pagesBufferLength: 2,
   },
-};
+}
 
 export default nextConfig;
