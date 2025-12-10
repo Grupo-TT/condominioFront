6775873494670564b329aabc4ed8c26fec1fdb3f
Paul Figueroa <paul.figueroa@like-a-startup.io>
2025-12-10 00:03:16 -0500

    Delete Solicitudes

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -60,11 +60,6 @@ const data = {
       url: "/asamblea",
       icon: <HugeiconsIcon icon={PresentationLineChart02Icon} size={18} strokeWidth={1.8} style={{ width: 18, height: 18 }} />,
     },
-    {
-      title: "Solicitudes",
-      url: "/solicitudes",
-      icon: <HugeiconsIcon icon={File02Icon} size={18} strokeWidth={1.8} style={{ width: 18, height: 18 }} />,
-    },
   ],
 }
 
